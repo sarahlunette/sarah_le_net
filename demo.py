5e8bb08cf974a2d33860ec1bfb786fe7da21ba3e
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-04-07 10:24:09 +0000

    main app commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+import streamlit as streamlit
+st.title('Sarah LE NET blogpost')
+pages = ['Natural Risks Prevention and Emergency Crisis', 'Health', 'Hackatons', 'Articles and Reposts', 'Teaching', 'Trips', 'Theater', 'Career', 'Entrepreneurship']
+p_interest = ['Interviews', 'State-of-the-art', 'Articles']
+hackatons = ['GeoHack AI','AI Action Summit', 'Women in Data Science']
+articles = ['Environment', 'Data and Tech', 'Entrepreneurship', 'Events and discoveries']
+teaching = ['Data Engineering', 'MLOPS', 'Data Science']
+trips = ['South America', 'Greece']
+career = ['Endeavor', 'CV', 'Certifications', 'University Projects', 'Portfolio']
+entrepreneurship = ['The Bubble', '?']
