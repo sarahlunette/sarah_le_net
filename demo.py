15f3207270c5c47e89b9ee01af9f9543a53ee408
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-04-19 00:05:45 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import streamlit as streamlit
 st.title('Sarah LE NET blogpost')
-pages = ['Natural Risks Prevention and Emergency Crisis', 'Health', 'Hackatons', 'Articles and Reposts', 'Teaching', 'Trips', 'Theater', 'Career', 'Entrepreneurship']
+pages = ['Natural Risks Prevention and Emergency Crisis', 'Health', 'Hackatons', 'Articles and Reposts', 'Teaching', 'Trips', 'Theater', 'Career', 'Entrepreneurship', 'Photography']
 p_interest = ['Interviews', 'State-of-the-art', 'Articles']
 hackatons = ['GeoHack AI','AI Action Summit', 'Women in Data Science']
 articles = ['Environment', 'Data and Tech', 'Entrepreneurship', 'Events and discoveries']
