59a2f1cd3ce4ac942208d9c1b650e9e430ce65e7
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-06-18 13:42:56 +0200

    english translation

--- demo.py
+++ demo.py
@@ -43,7 +43,8 @@ if sidebar==pages[2]:
 
 if sidebar==pages[10]:
     st.title('CNAM')
-    st.markdown(f'<a href="https://docs.google.com/presentation/d/1F1t67TK6D-fCbuYQ6IoT_T6-wQ2aN2eTbr2cA3qLWPI/edit?usp=sharing">📄 Ouvrir la présentation</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="https://docs.google.com/presentation/d/1F1t67TK6D-fCbuYQ6IoT_T6-wQ2aN2eTbr2cA3qLWPI/edit?usp=sharing">📄 Open presentation</a>', unsafe_allow_html=True)
+    
     st.header('Project Rendering (French Version)')
-    st.markdown(f'<a href="https://docs.google.com/document/d/165F-R3qm1RqhEpT7p1xB6oqRg3C_9l8VB-Bz-cO7gXg/edit?tab=t.0">📄 Ouvrir la présentation</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="https://docs.google.com/document/d/165F-R3qm1RqhEpT7p1xB6oqRg3C_9l8VB-Bz-cO7gXg/edit?tab=t.0">📄 Open rendering</a>', unsafe_allow_html=True)
 
