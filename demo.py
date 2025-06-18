10a0cd4e29344b30f3a0b1a62dc8a84302483fbb
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-06-18 06:56:17 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -43,7 +43,7 @@ if sidebar==pages[2]:
 
 if sidebar==pages[10]:
     st.title('CNAM')
-    st.header(f'<a href="https://docs.google.com/presentation/d/1F1t67TK6D-fCbuYQ6IoT_T6-wQ2aN2eTbr2cA3qLWPI/edit?usp=sharing">📄 Ouvrir la présentation</a>')
+    st.markdown(f'<a href="https://docs.google.com/presentation/d/1F1t67TK6D-fCbuYQ6IoT_T6-wQ2aN2eTbr2cA3qLWPI/edit?usp=sharing">📄 Ouvrir la présentation</a>')
     # Chemin vers ton fichier PDF
     pdf_path = "CNAM/projet_expoped.pdf"
 
