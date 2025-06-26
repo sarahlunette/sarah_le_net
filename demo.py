7d51f5301ddcf6bc8a8d271bf857f4f50b48813e
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-06-26 15:11:57 +0000

    Correct intro

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ import urllib.parse
 
 st.title('The Lab')
 st.header('by Sarah LE NET')
-st.markdown('Inspired by passions and interests, I started writing this blog while doign a state-of-the-art of prevention of natural risks and crises response as well as GenAI in healthcare. It turned out a lot of what has been done is valuable and I would love to present it to you.')
+st.markdown('Inspired by passions and interests, I started writing this blog while working on a state-of-the-art of data in prevention of natural risks and crises response as well as GenAI in Healthcare. It turned out a lot of what has been done is valuable and I would love to present it to you.')
 pages = ['Presentation', 'Hackathons', 'Articles', 'Teaching', 'Career', 'Entrepreneurship', 'CNAM: Metrology and Citizen Sciences', 'Trips', 'Theater', 'Photography']
 
 st.sidebar.markdown(
