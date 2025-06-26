912a2fd6c6e46877e6d7d50aff05910e20639cf3
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-06-26 15:27:33 +0000

    fix display of repo

--- demo.py
+++ demo.py
@@ -36,24 +36,36 @@ entrepreneurship = ['The Bubble', '?']
 # Récupérer la liste des fichiers .txt
 article_files = [f for f in os.listdir("articles") if f.endswith(".txt")]
 
+
+def display_contents(contents, indent=0):
+    for content in contents:
+        prefix = "•" if content.type == "dir" else "*"
+        spacer = " " * indent  # Use a Unicode EM SPACE for better alignment
+
+        if content.type == "dir":
+            st.markdown(f"{spacer}{prefix} {content.name}")
+            sub_contents = repo.get_contents(content.path)
+            display_contents(sub_contents, indent + 1)
+        else:
+            st.markdown(f"{spacer}{prefix} [{content.name}]({content.html_url})")
+
 if sidebar==pages[0]:
     st.title('The Lab')
     st.header('by Sarah LE NET')
     st.markdown('Inspired by passions and interests, I started writing this blog while working on a state-of-the-art of data in prevention of natural risks and crises response as well as GenAI in Healthcare. It turned out a lot of what has been done is valuable and I would love to present it to you.')
 
-if sidebar==pages[1]:
+if sidebar == pages[1]:
     selected_hackathons = st.selectbox("Choose a hackathon", hackathons)
 
-    if selected_hackathons==hackathons[0]:
-        g = Github()  # or Github("your_token") for private repos
+    if selected_hackathons == hackathons[0]:
+        g = Github()  # optionally use Github("your_token") if private
         repo = g.get_repo("sarahlunette/GeoAIHack_team_18")
 
         st.title(repo.name)
         st.write(repo.description)
 
-        contents = repo.get_contents("")
-        for file in contents:
-            st.write(f"{file.type.upper()}: [{file.name}]({file.html_url})")
+        root_contents = repo.get_contents("")
+        display_contents(root_contents)
 
 
 # Afficher une liste déroulante pour choisir un article
