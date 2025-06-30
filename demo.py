67c44612dd2bb71ce3e39ec9ec4bcf9910c86f08
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-06-30 09:57:17 +0200

    update demo selectbox

--- demo.py
+++ demo.py
@@ -83,9 +83,10 @@ import streamlit.components.v1 as components
 
 # Afficher une liste déroulante pour choisir un article
 if sidebar == pages[2]:
-    st.sidebar.selectbox("Choose an article", article_files)
     article_files = [f.replace(".html", "") for f in os.listdir("articles") if f.endswith(".html")]
-    selected_article = st.selectbox("Choose an article", article_files)
+    st.sidebar.selectbox("Choose an article", article_files)
+
+    # selected_article = st.selectbox("Choose an article", article_files)
 
     # Charger et afficher l'article HTML sélectionné
     with open(f"articles/{selected_article}.html", "r", encoding="utf-8") as file:
