67c141ad41f2cf353243b24e1ac100e16f9e08b0
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-08-28 08:39:53 +0200

    update resume format to pdf

--- demo.py
+++ demo.py
@@ -186,7 +186,7 @@ if sidebar == pages[4]:
 
     if selected_item == "Resume":
         st.header("Resume ")
-        st.image("images/resume.jpg")
+        st.image("images/resume.pdf")
 
     elif selected_item == "Certifications":
         st.header("Google Professional Data Engineer Certifications")
