e9285edf8bf68a383b8dc459fb578ea9680b33cb
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-08-28 07:47:41 +0200

    update test photo demo

--- demo.py
+++ demo.py
@@ -5,6 +5,7 @@ import urllib.parse
 from github import Github
 from dotenv import load_dotenv
 from github import Auth
+import requests
 
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -203,8 +204,9 @@ if sidebar == pages[7]:
 
 
 if sidebar == pages[10]:
+
     image_urls = [
-        ("https://photos.google.com/share/AF1QipOLjhoHeuXLtSEvSbE87LHrOJMZPZf7NYBg42Tby8U9mH95s_ZxgixajdjFwBv33A/photo/AF1QipOXgaB2IOItZksUXchCGSKMugULRUcIq3RKD2yH?key=aXMwVjJVakRtUmxhTkprLVpkanlmQXdCM20zM0x3", "Stoa, Athina"),
+        ("https://photos.app.goo.gl/bigTvRrEHo64ujak8", "Stoa, Athina"),
         ("https://photos.app.goo.gl/VKbWruWGHw1Aw6kQ7", "Agio Georgious, Thessaloniki"),
         ("https://photos.app.goo.gl/JadDRxCH7VT37xNh7", "Antique theater, Athina"),
         ("https://photos.app.goo.gl/TKcGr9jMFPGVM6Mx9", "Statue"),
@@ -221,5 +223,5 @@ if sidebar == pages[10]:
     ]
 
     for image_url, caption in image_urls:
-        print(image_url, caption)
-        st.image(image_url, caption=caption)
+        response = requests.get(image_url)
+        st.image(response.content, caption=caption)
