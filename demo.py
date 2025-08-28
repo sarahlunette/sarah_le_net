772f85a577212c609fd3559ca85f91258e4e8830
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-08-28 08:51:12 +0200

    test update pdf display code

--- demo.py
+++ demo.py
@@ -189,9 +189,8 @@ if sidebar == pages[4]:
         st.header("Resume ")
         # Path to your PDF
         pdf_file = "images/resume.pdf"
-        with open(pdf_file,"rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        
+        bytes_data = pdf_file.read()
+        base64_pdf = base64.b64encode(bytes_data.read()).decode('utf-8')
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'   
         st.markdown(pdf_display, unsafe_allow_html=True)
 
