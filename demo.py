dc8cdcbbf5753ae4b7fcacb836c5e133c69ae5cb
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-12-12 16:33:48 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -104,6 +104,7 @@ if sidebar == pages[0]:
     st.markdown(
         "Inspired by passions and interests, I started writing this blog while working on a state-of-the-art of data in prevention of natural risks and crises response as well as GenAI in Healthcare. In the last section I'll present some tech watch and culture events. It turned out a lot of what has been done is valuable and I would love to present it to you."
     )
+    st.write('I started this journey a long time ago, inspired by nature, people and physics. At Columbia University, I got out of my training with a will to use data for good. I then continued into environmental issues and decided to go on a Long trip across South America to discover landscape, problems and technology possibilities. 4 years ago I encountered DataKind whom I have followed since and when GenAI appeared mainstream, I started asking questions on how to apply it to disaster preparedness, emergency crisis, NGOs, etc. I looked from far organisations such as Global Pulse or Google Crisis Response.')
 
 if sidebar == pages[1]:
     st.write("The entirety of these hackathons are directed towards emergency crises and resilience or healthcare as well as help to development, or towards specific technologies: remote sensing, VIT, NLP, Embeddings, LLM and Agents.")
