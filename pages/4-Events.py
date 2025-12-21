21c21791798d999ad215a2455064c9f24fae609c
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-12-21 16:55:11 +0100

    events

--- pages/4-Events.py
+++ pages/4-Events.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+import streamlit.components.v1 as components
 
 events = [
     "Climate House",
@@ -25,3 +27,13 @@ events = [
 st.title("Events")
 for event, i in zip(events, range(1, len(events) + 1)):
     st.write(f"{i} - {event}")
+
+events = st.sidebar.radio("Choose an event", ["datacraft"])
+if events == "datacraft":
+    with open(
+        f"events/{events}.html", "r", encoding="utf-8"
+    ) as file:
+        html_content = file.read()
+    st.title(events.replace("_", " "))
+    components.html(html_content, height=800, scrolling=True)
+
