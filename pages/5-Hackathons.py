775710fe71df6c3da4275b6e3ef0371be6dda395
Sarah Le Net <sarahlunette@users.noreply.github.com>
2026-01-03 08:19:47 +0100

    hackathons

--- pages/5-Hackathons.py
+++ pages/5-Hackathons.py
@@ -25,8 +25,8 @@ hackathons = [
     "HackTheGap",
     "IteratexColumbia",
     "Manus",
-    "DevPost",
-    "Personal Project"
+    "DevPost: A social media cartography of dangers with Confluent",
+    "Personal Project: Research on LLM responses to transcription of personal journals"
 ]
 repo_names = [
     "GeoAIHack_team_18",
@@ -47,8 +47,8 @@ repo_names = [
     "HackTheGap",
     "Hackathon-Columbia",
     "navigating_through_crises",
-    "devpost",
-    "transcription_ocr"
+    "DevPost",
+    "Transcription_OCR"
 ]
 
 st.title("Hackathons")
